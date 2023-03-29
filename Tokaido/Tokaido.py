70fa0cee65649902e10622ac0d97e9e67e9d5d1d
gaetan quenouille <gaetan.quenouille.gq@gmail.com>
2023-03-29 17:47:06 +0200

    je crois ça marche pas hein

--- Tokaido/Tokaido.py
+++ Tokaido/Tokaido.py
@@ -1,22 +1,7 @@
-from Class.Auth import Auth
+from Class.Authenticator import *
 import pygame
 
 """Creation du plateau"""
 case = []
 for i in range (54):
     case.append(i)
-
-id_mdp = {}
-id_mdp["anateg"] = "20020417"
-id_mdp[" rzejnkqfn bjidlsb qkchjsgqvjklhfkjgvns"] = "eusihferiduvqhfiuogdsohnijolbhns"
-
-
-"""Authentification en mode console (retirer les input pour adapter en pygame)"""
-
-userid = input("id : ").lower()
-while userid not in id_mdp :
-    userid = input("Cet utilisateur n'est pas enregistre\nSaisissez-en un autre : ").lower()
-
-mdp = input("mdp : ")
-while Auth.verif(userid, mdp, id_mdp) != 0 :
-    mdp = input("Mot de passe incorrect\nSaissiez un autre mot de passe : ")
