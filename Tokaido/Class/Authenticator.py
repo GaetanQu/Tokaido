3dab212888951e3544decb21c20c6f8504344504
gaetan quenouille <gaetan.quenouille.gq@gmail.com>
2023-03-29 19:00:17 +0200

    Authentificateur termine

--- Tokaido/Class/Authenticator.py
+++ Tokaido/Class/Authenticator.py
@@ -2,8 +2,10 @@
 import PySimpleGUI as sg
 import csv
 
+csv_directory = "Class/users.csv"
+
 #Importation des donnees du fichier csv
-with open("tests/users.csv", mode = "r") as file :
+with open(csv_directory, mode = "r") as file :
     filereader = csv.reader(file, delimiter=';')
     user = {}
     for ligne in filereader :
@@ -14,27 +16,30 @@ with open("tests/users.csv", mode = "r") as file :
 sg.theme('GrayGrayGray')
 
 #Creation du layout de la fenetre de connexion
-login_layout = [
-    [sg.Text("Identifiez vous pour jouer au Tokaido !", font = ('Comic Sans MS',20))],
-    [sg.Text("")],
-    [sg.Text("Nom d'utilisateur : "), sg.InputText(key = "-ID-")],
-    [sg.Text("Mot de pase :       "), sg.InputText(key = "-MDP-")],
-    [sg.Button("OK"), sg.Text("", key = "-WRONG-")],
-    [sg.Button("Creer un compte")]
-    ]
-login_window = sg.Window("Connectez-vous", login_layout, finalize=True)
+login_title = [[sg.Text("Connectez-vous pour jouer au Tokaido !", font = ("Arial", 20))]]
+login_lcol = sg.Column([[sg.Text("Nom d'utilisateur")],
+                        [sg.Text("Mot de passe")]])
+login_rcol = sg.Column([[sg.InputText(key="-ID-")],
+                        [sg.InputText(key="-MDP-", password_char="*")]])
+login_buttons = [[sg.Button("Connexion"), sg.Button("Creer un compte"), sg.Text("", key="-WRONG-")]]
+
+#Creation de la fenetre de connexion
+login_window = sg.Window("Connectez-vous", [[login_title],[login_lcol, login_rcol], login_buttons ], finalize=True)
 
 #Creation du layout de la fenetre d'inscription
-register_layout = [
-    [sg.Text("Creez un compte", font = ("Arial", 20))],
-    [sg.Text("")],
-    [sg.Text("Nom d'utilisateur : "), sg.InputText(key = "-NEWID-")],
-    [sg.Text("Mot de passe :      "), sg.InputText(key = "-NEWMDP-")],
-    [sg.Text("Confirmation mdp :  "), sg.InputText(key = "-MDPCONFIRM-")],
-    [sg.Text("", key = "-NEWWRONG-")],
-    [sg.Button("Creer mon compte et me connecter"), sg.Button("J'ai deja un compte, me connecter")],
-    ]
-register_window = sg.Window("Insrcivez-vous", register_layout, finalize=True)
+register_title = [[sg.Text("S'inscrire", font = ("Arial", 20))]]
+register_lcol = sg.Column([[sg.Text("Nom d'utilisateur")],
+                           [sg.Text("Mot de passe")],
+                           [sg.Text("Confirmation du mot de passe")]])
+register_rcol = sg.Column([[sg.InputText(key="-NEWID-")],
+                           [sg.InputText(key="-NEWMDP-", password_char="*")],
+                           [sg.InputText(key="-MDPCONFIRM-", password_char="*")]])
+register_wrong = [[sg.Text("", key="-NEWWRONG-")]]
+register_buttons = [[sg.Button("Creer un compte et me connecter"), sg.Button("J'ai deja un compte, me connecter")]]
+
+#Creation de la fenetre d'inscription
+register_window = sg.Window("Insrcivez-vous", [[register_title], [register_lcol, register_rcol],[register_wrong], [register_buttons]], finalize=True)
+
 #Comme on ne s'inscrit qu'une fois, on affiche par defaut la fenetre de connexion, et donc on cache la fenetre d'inscription
 register_window.hide()
 
@@ -46,17 +51,17 @@ def auth() :
             break
 
         #Connexion
-        if window == login_window and event == "OK" :
-            if values["-ID-"] in user and values["-MDP-"] == user[values["-ID-"]][1] :
+        if window == login_window and event == "Connexion" :
+            if values["-ID-"].lower() in user and values["-MDP-"] == user[values["-ID-"].lower()][1] :
                 break
             else :
-                window["-WRONG-"].update("ID ou MDP incorrect")
+                window["-WRONG-"].update("Identifiant ou mot de passe incorrect")
 
         #Inscription
-        if window == register_window and event == "Creer mon compte et me connecter" :
+        if window == register_window and event == "Creer un compte et me connecter" :
 
             #Verification que l'utilisateur n'est pas deja enregistre
-            if values["-NEWID-"] in user :
+            if values["-NEWID-"].lower() in user :
                 window["-NEWWRONG-"].update("Cet utilisateur est deja enregistre")
 
             #Verification de l'exactitude du mot de passe
@@ -65,15 +70,14 @@ def auth() :
 
             #Ajout de l'utilisateur si tout est bon
             else:
-                with open("tests/users.csv", mode = "a", newline = "") as file :
+                with open(csv_directory, mode = "a", newline = "") as file :
                     filewriter = csv.writer(file, delimiter = ";")
                     #L'utilisateur aura pour donnees de victoire et de defaites 0, car il vient de s'inscrire
-                    filewriter.writerow([str(values["-NEWID-"]), int(values["-NEWMDP-"]), 0, 0])
+                    filewriter.writerow([str(values["-NEWID-"].lower()), str(values["-NEWMDP-"]), 0, 0])
                     file.close()
                 break
 
         #Passage de la fenetre de connexion a la fenetre d'inscription
-        """penser a initialiser la position de la fenetre a chaque fois, de telle sorte que si l'utilisateur change de fenetre, il ne se retrouve pas teleporte d'un morceau d'ecran a l'autre"""
         if window == login_window and event == "Creer un compte" :
             login_window.hide()
             register_window.un_hide()
--- Tokaido/Tokaido.py
+++ Tokaido/Tokaido.py
@@ -1,7 +1,16 @@
-from Class.Authenticator import *
+"""
+Voici le code principal du programme. C'est ici que seront exécutées les fonctions créées dans les différents fichiers.
+"""
+
+#Importation de tous les modules et classes nécessaires au bon fonctionnement du programme
+import Class.Authenticator
 import pygame
 
-"""Creation du plateau"""
+#Creation du plateau
 case = []
 for i in range (54):
     case.append(i)
+
+#En premier lieu, l'utilisateur doit s'identifier
+Class.Authenticator.auth()
+
