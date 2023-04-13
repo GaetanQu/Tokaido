76e9da1553ecbaab4802c2332c77b560d708d708
gaetan quenouille <gaetan.quenouille.gq@gmail.com>
2023-04-13 16:15:21 +0200

    None

--- Tokaido/Class/menu.py
+++ Tokaido/Class/menu.py
@@ -1,22 +1,35 @@
+from tkinter import CENTER
 import pygame
 pygame.init()
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
 BG_COLOR = (251,253,248)
+BUTTON_WIDTH = 200
+BUTTON_HEIGHT = 60
+BUTTON_TEXT_COLOR = BLACK
+BUTTON_BACKGROUND_COLOR = WHITE
+
+#play_button = pygame.Rect(100, 100, BUTTON_WIDTH, BUTTON_HEIGHT)
 
 class Menu():
     def __init__ (self) :
         pass
 
     def launch(self):
-        bg = pygame.image.load('Tokaido/class/images/menu/bg.jpg')
-
-        bg_width, bg_height = bg.get_size()
-
-        screen = pygame.display.set_mode((0,0))
-
-        pygame.transform.scale(bg, (bg_width,bg_height))
-
+        screen = pygame.display.set_mode((1500, 720))
         screen_width = screen.get_width()
         screen_height = screen.get_height()
+
+        CENTER = (screen_width/2 ,screen_height/2)
+        CENTERW = screen_width/2
+        CENTERH = screen_height/2
+
+        bg = pygame.image.load('Tokaido/class/images/menu/bg.jpg')
+        bg_width, bg_height = bg.get_size()
+        BGCENTER = (CENTERW - bg_width/2 ,CENTERH - bg_height/2)
+
 
         pygame.display.set_caption("Menu Tokaido")
         while True :
@@ -25,9 +38,10 @@ class Menu():
                     pygame.quit()
                     break
             screen.fill(BG_COLOR)
-            screen.blit(bg, ((screen_width - bg_width)/2 ,(screen_height - bg_height)/2))
+            screen.blit(bg, BGCENTER)
+            #screen.blit(play_button, (CENTERW, (screen_height + CENTERH)/2))
             pygame.display.flip()
         pass
 
 menu = Menu()
-menu.launch()   
+menu.launch()
