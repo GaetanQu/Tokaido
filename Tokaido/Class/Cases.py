81788929e31302f2b019b55aad881bca5065ed49
Tristan DEHON <tristan.dehon@groupe-esigelec.org>
2023-04-14 15:49:23 +0200

    comme le dernier,

--- Tokaido/Class/Cases.py
+++ Tokaido/Class/Cases.py
@@ -8,10 +8,10 @@ class Cartes :
         self.echoppe_cases=[1,8,25,29,40,45,53]
         self.echoppe_cartes=[{},{},{},{}]
 
-        #Tu pars du principe que dico[emplacement] = [points_rapportes, prix cartes00]
+        #Tu pars du principe que dico[emplacement] = [points_rapportes, prix cartes]
 
         self.source_cases=[5,13,22,33,42,48]
-        self.source_cartes={emplacement:[points_rapportes, prix_carte]}            {'Bureau/pygame/images/nom_image'[[3, 2]]}
+        self.source_cartes={'emplacement':['points_rapportes', 'prix_carte']}            #{'Bureau/pygame/images/nom_image'[[3, 2]]}
 
         self.rencontre_cases=[3,10,20,30,38,44,49]
         self.rencontre_cartes={}
@@ -47,28 +47,35 @@ class Cartes :
             
     def tirage(self, nb_cartes_a_tirer, joueur): 
 
-        liste_cartes_possibles=self.test_case(joueur)      #afin de ne pas editer la liste initiale 
-        cartes_a_proposer=[]                        #liste des cartes qui seront proposees au joueur
+        liste_cartes_possibles=self.test_case(joueur)                                   #afin de ne pas editer la liste initiale 
+
 
             
-        if joueur.case in self.pano_cases[0]:
-            for i in range (len(liste_cartes_possibles)):
-                if liste_cartes_possibles[i] not in joueur.cartes_pano[0]:      #on ajoute la carte pano ssi le joueur ne la pas encore
-                    joueur.cartes_pano[0].append(liste_cartes_possibles[i])     #(car les cartes pano se recoivent dans lordre)
-                    joueur.points+=self.pano_cartes[0][liste_cartes_possibles[i]][0]
-                    return
-
-        elif joueur.case in self.pano_cases[1]:
-            for i in range (len(liste_cartes_possibles)):
-                if liste_cartes_possibles[i] not in joueur.cartes_pano[1]:      
-                    joueur.cartes_pano[1].append(liste_cartes_possibles[i])           
+        if joueur.case in self.pano_cases[0]:                                           #SI CASE = PANO MER
+            indice=0
+            while liste_cartes_possibles[indice] in joueur.cartes_pano[0] :
+                indice+=1
+            if indice<=len(liste_cartes_possibles):                                     #on ajoute la carte pano ssi le joueur ne la pas encore
+                joueur.cartes_pano[0].append(liste_cartes_possibles[indice])            #(car les cartes pano se recoivent dans lordre)
+                joueur.points+=self.pano_cartes[0][liste_cartes_possibles[indice]][0]
 
 
-        elif joueur.case in self.pano_cases[2]:
-            for i in range (len(liste_cartes_possibles)):
-                if liste_cartes_possibles[i] not in joueur.cartes_pano[2]:      
-                    joueur.cartes_pano[2].append(liste_cartes_possibles[i])  
+        elif joueur.case in self.pano_cases[1]:                                         #SI CASE = PANO MONTAGNE
+            indice=0
+            while liste_cartes_possibles[indice] in joueur.cartes_pano[1] :
+                indice+=1
+            if indice<=len(liste_cartes_possibles):                                     
+                joueur.cartes_pano[1].append(liste_cartes_possibles[indice])            
+                joueur.points+=self.pano_cartes[1][liste_cartes_possibles[indice]][0]   #[0] car les points sont stockés en 1er rang dans le dico.
 
+
+        elif joueur.case in self.pano_cases[3]:
+            indice=0
+            while liste_cartes_possibles[indice] in joueur.cartes_pano[2] :
+                indice+=1
+            if indice<=len(liste_cartes_possibles):                                     
+                joueur.cartes_pano[2].append(liste_cartes_possibles[indice])            
+                joueur.points+=self.pano_cartes[2][liste_cartes_possibles[indice]][0]
 
 
 
@@ -79,11 +86,16 @@ class Cartes :
                     del(liste_cartes_possibles[i])
                 i+=1                     
         
+        elif joueur.case in self.rencontre_cases:
+            i=0
+            for carte in liste_cartes_possibles:    
+                if carte in joueur.cartes_rencontre:
+                    del(liste_cartes_possibles[i])
+                i+=1   
                 
-                
-                
-            for i in range (nb_cartes_a_tirer):         #a utiliser later
-                carte=Random.randint(1,len(liste_cartes_possibles))
-                cartes_a_proposer.append(liste_cartes_possibles[carte])
+        cartes_a_proposer=[]                                                            #liste des cartes qui seront proposees au joueur               
+        for i in range (nb_cartes_a_tirer):                                 #constitution de la liste des cartes à proposer
+            indice_carte=Random.randint(1,len(liste_cartes_possibles))
+            cartes_a_proposer.append(liste_cartes_possibles[indice_carte])
         
             
--- Tokaido/Class/Joueur.py
+++ Tokaido/Class/Joueur.py
@@ -3,12 +3,14 @@ class Joueur:
         self.couleur=couleur
         self.personnage=personnage
         self.pieces=personnage.pieces
-        self.cartes_pano=[[],[],[]]    #mer, montagne, riziere
+        self.cartes_pano=[[],[],[]]             #mer, montagne, riziere
         self.cartes_repas=[]
-        self.cartes_echoppe[[],[],[],[]]   #4 familles de cartes, faudrait les mettre dans l'ordre 
+        self.cartes_echoppe[[],[],[],[]]        #4 familles de cartes, faudrait les mettre dans l'ordre 
         self.pieces_donnees_temple=0
         self.pieces_depensees_repas=0
         self.case=0
         self.points=0
+        self.achievements=[0,0,0,0,0,0,0,0]     #pano_montagne, pano_rizière, temples, repas, source chaude, rencontre, souvenir 
+                                                #on remplace pas 1 si le mec a l'achievment.
     
 
