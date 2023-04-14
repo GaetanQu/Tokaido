092c83c630d93325c26f1e13c4b7fa4f2ca17f84
gaetan quenouille <gaetan.quenouille.gq@gmail.com>
2023-04-14 15:50:33 +0200

    hover_account

--- Tokaido/Class/menu.py
+++ Tokaido/Class/menu.py
@@ -17,6 +17,11 @@ bg_width, bg_height = bg.get_size()
 title = pygame.image.load('Tokaido/class/images/menu/title.png')
 title_width, title_height = title.get_size()
 
+account = pygame.image.load('Tokaido/class/images/menu/account.png')
+account = pygame.transform.scale(account, (75,75))
+
+hovered_account = pygame.image.load('Tokaido/class/images/menu/hovered_account.png')
+
 #play_button = pygame.Rect(100, 100, BUTTON_WIDTH, BUTTON_HEIGHT)
 
 class Menu():
@@ -34,18 +39,28 @@ class Menu():
         BGCENTER = (CENTERW - bg_width/2 ,CENTERH - bg_height/1.5)
         TITLEPOS = (CENTERW - title_width/2, screen_height - 2*title_height)
 
+
         pygame.display.set_caption("Menu Tokaido")
         while True :
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     break
+
             screen.fill(BG_COLOR)
             screen.blit(bg, BGCENTER)
             screen.blit(title, TITLEPOS)
-            #screen.blit(play_button, (CENTERW, (screen_height + CENTERH)/2))
+
+            account_rect = account.get_rect()
+            account_rect.center = 30+75/2, 30+75/2
+
+            if account_rect.collidepoint(pygame.mouse.get_pos()):
+                screen.blit(hovered_account, (30,30))
+            else :
+                screen.blit(account, (30,30))
+
             pygame.display.flip()
         pass
 
-#menu = Menu()
-#menu.launch()
+menu = Menu()
+menu.launch()
